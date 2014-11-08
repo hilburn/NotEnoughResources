8dd895a10fcd6c5ea56bf668ce8276822b1c46d3
hilburn <charliepaterson89@gmail.com>
2014-11-08 22:08:23 +0000

    more mod-ly stuff
    cofhcore compat begun

--- src/main/java/neiresources/NEIResources.java
+++ src/main/java/neiresources/NEIResources.java
@@ -1,4 +1,20 @@
 package main.java.neiresources;
 
-public class NEIResources {
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import main.java.neiresources.compatibility.cofh.CoFHCompat;
+
+@Mod(modid = "neiresources", name = "NEI Resources", version = "NEIRESOURCES_VER", dependencies = "after:NotEnoughItems")
+public class NEIResources
+{
+
+    @Instance(value = "neiresource")
+    public static NEIResources INSTANCE;
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+        new CoFHCompat();
+    }
 }
