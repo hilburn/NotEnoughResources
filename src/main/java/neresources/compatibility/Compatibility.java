f5f02c2b2d21cbefbad0f7a1fbbc90aa3e4d3353
hilburn <charliepaterson89@gmail.com>
2014-11-17 19:27:29 +0000

    bloody compatibility.java

--- src/main/java/neresources/compatibility/Compatibility.java
+++ src/main/java/neresources/compatibility/Compatibility.java
@@ -1,5 +1,13 @@
 package neresources.compatibility;
 
+import neresources.api.NEResourcesAPI;
+import neresources.api.entry.IDungeonEntry;
+import neresources.api.entry.IMobEntry;
+import neresources.api.entry.IOreEntry;
+import neresources.registry.DungeonRegistry;
+import neresources.registry.GrassSeedRegistry;
+import neresources.registry.MobRegistry;
+import neresources.registry.OreRegistry;
 import neresources.utils.ModList;
 
 public class Compatibility
@@ -10,6 +18,13 @@ public class Compatibility
         for (ModList mod : ModList.values())
         {
             mod.initialise();
+        }
+
+        for (Object entry: NEResourcesAPI.getRegistryAPI().values())
+        {
+            if (entry instanceof IMobEntry) MobRegistry.getInstance().registerMob((IMobEntry)entry);
+            else if (entry instanceof IDungeonEntry) DungeonRegistry.getInstance().registerDungeonEntry((IDungeonEntry) entry);
+            else if (entry instanceof IOreEntry) OreRegistry.getInstance().registerOre(((IOreEntry)entry));
         }
     }
 }
