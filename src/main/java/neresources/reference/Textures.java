b6b1b0b8fa8b37b220258569efb605759ccb8e63
way2muchnoise <jens.baetens@live.be>
2014-11-25 23:02:04 +0100

    fixes the missing textures in the builds
    hopefully

--- src/main/java/neresources/reference/Textures.java
+++ src/main/java/neresources/reference/Textures.java
@@ -6,10 +6,10 @@ public final class Textures
     {
         private static final String GUI_DIR = "textures/gui/";
 
-        public static final String MOB_NEI = GUI_DIR + "NEIMobGui.png";
-        public static final String ORE_NEI = GUI_DIR + "NEIOreGui.png";
+        public static final String MOB_NEI = GUI_DIR + "NEIMobGUI.png";
+        public static final String ORE_NEI = GUI_DIR + "NEIOreGUI.png";
         public static final String DUNGEON_NEI = GUI_DIR + "NEIDungeonGUI.png";
-        public static final String GRASS_NEI = GUI_DIR + "NEIGrassGui.png";
+        public static final String GRASS_NEI = GUI_DIR + "NEIGrassGUI.png";
         public static final String SEED_NEI = GUI_DIR + "NEISeedGUI.png";
         public static final String ENCHANTMENT_NEI = GUI_DIR + "NEIEnchantmentGUI.png";
     }
