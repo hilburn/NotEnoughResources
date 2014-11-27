4e18533c433fcb466aff8062f80eb474ad10322f
hilburn <charliepaterson89@gmail.com>
2014-11-27 14:18:16 +0000

    ReactorCraft Compat

--- src/main/java/neresources/compatibility/reika/ReactorCraftCompat.java
+++ src/main/java/neresources/compatibility/reika/ReactorCraftCompat.java
@@ -1,7 +1,35 @@
 package neresources.compatibility.reika;
 
+import Reika.ReactorCraft.Registry.ReactorOres;
+import neresources.api.NEResourcesAPI;
+import neresources.api.distributions.DistributionSquare;
 import neresources.compatibility.CompatBase;
+import neresources.registry.AddOreDrop;
+import neresources.registry.OreEntry;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 public class ReactorCraftCompat extends CompatBase{
-
+    @Override
+    protected void init() {
+        for (ReactorOres reactorOre:ReactorOres.oreList)
+        {
+            boolean generates = reactorOre.shouldGen || !reactorOre.hasEquivalents();
+            int metadata = reactorOre.getBlockMetadata();
+            ItemStack ore = new ItemStack(reactorOre.getBlock(),1,metadata);
+            List<ItemStack> drops = reactorOre.getOreDrop(metadata);
+            for (ItemStack drop:drops)
+            {
+                NEResourcesAPI.registerEntry(new AddOreDrop(ore,drop));
+            }
+            int minY = reactorOre.minY;
+            int maxY = reactorOre.maxY;
+            int numVeins = reactorOre.perChunk;
+            int veinSize = reactorOre.veinSize;
+            float chance = (numVeins*veinSize)/((maxY-minY+1)*256);
+            if (!generates) continue;
+            registerOre(new OreEntry(ore,new DistributionSquare(Math.max(0, minY - veinSize / 2), minY, maxY, Math.min(maxY + veinSize / 2, 255), chance)));
+        }
+    }
 }
