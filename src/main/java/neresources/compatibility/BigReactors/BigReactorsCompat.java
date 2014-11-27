b7aa3d97c4dcd3b65c2260eb5e7d35af405a2706
hilburn <charliepaterson89@gmail.com>
2014-11-27 15:56:36 +0000

    Temp fix for BR

--- src/main/java/neresources/compatibility/BigReactors/BigReactorsCompat.java
+++ src/main/java/neresources/compatibility/BigReactors/BigReactorsCompat.java
@@ -20,18 +20,20 @@ public class BigReactorsCompat extends CompatBase {
         if (oreGeneratorSet==null) return;
         for (BRSimpleOreGenerator oreGen:oreGeneratorSet)
         {
+            if (oreGen==null) continue;
             int minY = ReflectionHelper.getInt(BRSimpleOreGenerator.class,"minY",oreGen);
             int maxY = ReflectionHelper.getInt(BRSimpleOreGenerator.class,"maxY",oreGen);
             int minVeins = ReflectionHelper.getInt(BRSimpleOreGenerator.class,"minClustersPerChunk",oreGen);
             int maxVeins = ReflectionHelper.getInt(BRSimpleOreGenerator.class,"maxClustersPerChunk",oreGen);
             boolean isObf = ReflectionHelper.doesFieldExist(WorldGenMinable.class, "field_76541_b");
-            int veinSize = ReflectionHelper.getInt(WorldGenMinable.class, isObf ? "field_76541_b" : "numberOfBlocks",oreGen);
+            System.out.println("Obfuscated Minecraft: "+isObf);
+            int veinSize = 10;//ReflectionHelper.getInt(WorldGenMinable.class, isObf ? "field_76541_b" : "numberOfBlocks",oreGen).intValue();
             Block oreBlock = (Block) ReflectionHelper.getObject(BRSimpleOreGenerator.class,"blockToGenerate",oreGen);
             Block toReplace = (Block) ReflectionHelper.getObject(BRSimpleOreGenerator.class,"blockToReplace",oreGen);
             int metadata = ReflectionHelper.getInt(BRSimpleOreGenerator.class,"blockToGenerateMetadata",oreGen);
             Set<Integer> dimensionBlacklist = (Set<Integer>) ReflectionHelper.getObject(BRSimpleOreGenerator.class,"dimensionBlacklist",oreGen);
             ItemStack ore = new ItemStack(oreBlock,1,metadata);
-            float chance = (float)((minVeins+maxVeins)/2)*veinSize/((maxY-minY+1)*256);
+            float chance = ((float)(minVeins+maxVeins)/2)*veinSize/((maxY-minY+1)*256);
             OreEntry entry = new OreEntry(ore,new DistributionSquare(Math.max(0, minY - veinSize / 2), minY, maxY, Math.min(maxY + veinSize / 2, 255), chance));
             registerOre(entry);
         }
