a46a11fee1b21fd3df413eb785c9d2be88aefe22
hilburn <charliepaterson89@gmail.com>
2014-11-29 15:45:09 +0000

    Priotiry

---
+++ src/main/java/neresources/api/utils/messages/OreMessages.java
@@ -0,0 +1,16 @@
+package neresources.api.utils.messages;
+
+import neresources.registry.OreEntry;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class OreMessages
+{
+
+    public static NBTTagCompound getRegisterOreMessage(OreEntry entry)
+    {
+        NBTTagCompound message = new NBTTagCompound();
+
+        return message;
+    }
+
+}
