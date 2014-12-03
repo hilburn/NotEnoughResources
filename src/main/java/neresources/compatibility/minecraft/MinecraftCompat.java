1f3e70c0dea0be5cb20e4abc959583ef16ebb874
hilburn <charliepaterson89@gmail.com>
2014-12-03 17:31:39 +0000

    Removed test

--- src/main/java/neresources/compatibility/minecraft/MinecraftCompat.java
+++ src/main/java/neresources/compatibility/minecraft/MinecraftCompat.java
@@ -43,7 +43,6 @@ public class MinecraftCompat extends CompatBase
     private void registerVanillaOreDrops()
     {
         registerOre(new RegisterOreMessage(new ItemStack(Blocks.clay), new DistributionUnderWater(0.0035F), new ItemStack(Items.clay_ball,4)));
-        registerOre(new RegisterOreMessage(new ItemStack(Blocks.clay), new DistributionUnderWater(0.0035F), Restriction.OVERWORLD, new ItemStack(Items.clay_ball,4)));
         MessageRegistry.addMessage(new ModifyOreMessage(new ItemStack(Blocks.clay), Priority.FIRST, new ItemStack(Items.clay_ball, 4)));
         MessageRegistry.addMessage(new ModifyOreMessage(new ItemStack(Blocks.coal_ore), Priority.FIRST, new ItemStack(Items.coal)));
         MessageRegistry.addMessage(new ModifyOreMessage(new ItemStack(Blocks.diamond_ore), Priority.FIRST, new ItemStack(Items.diamond)));
