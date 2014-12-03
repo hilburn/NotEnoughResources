8b4dc0f5709c93b858ca7b1b412ecb9e6b4a54f0
hilburn <charliepaterson89@gmail.com>
2014-12-03 00:43:44 +0000

    More Merge

--- src/main/java/neresources/compatibility/CompatBase.java
+++ src/main/java/neresources/compatibility/CompatBase.java
@@ -1,9 +1,13 @@
 package neresources.compatibility;
 
+<<<<<<< HEAD
 import neresources.entries.MobEntry;
 import neresources.registry.MobRegistry;
 import neresources.entries.OreEntry;
 import neresources.registry.OreRegistry;
+=======
+import neresources.registry.*;
+>>>>>>> origin/master
 import neresources.utils.LogHelper;
 import neresources.utils.ModList;
 
@@ -35,5 +39,10 @@ public class CompatBase
     public void registerOre(OreEntry entry)
     {
         OreRegistry.getInstance().register(entry);
+    }
+
+    public void registerPlant(PlantEntry entry)
+    {
+        PlantRegistry.getInstance().registerPlant(entry);
     }
 }
