23b00f1fefb14cd1b65cc3372342a0f10a77766c
way2muchnoise <jens.baetens@live.be>
2014-12-14 17:28:18 +0100

    adds ic2 support
    needs work

---
+++ src/main/java/neresources/compatibility/ic2/IC2Compat.java
@@ -0,0 +1,66 @@
+package neresources.compatibility.ic2;
+
+import cpw.mods.fml.common.Optional;
+import ic2.core.Ic2Items;
+import ic2.core.init.MainConfig;
+import ic2.core.util.ConfigUtil;
+import neresources.api.distributions.DistributionSquare;
+import neresources.api.messages.RegisterOreMessage;
+import neresources.compatibility.CompatBase;
+import neresources.utils.ModList;
+
+public class IC2Compat extends CompatBase
+{
+    @Override
+    protected void init()
+    {
+        registerIC2Ores();
+    }
+
+    @Optional.Method(modid = ModList.Names.IC2)
+    private void registerIC2Ores()
+    {
+        float density = ConfigUtil.getFloat(MainConfig.get(), "worldgen/oreDensityFactor");
+        int baseScale = Math.round(64 * density);
+        int numVeins, minY, maxY, veinSize;
+        if(ConfigUtil.getBool(MainConfig.get(), "worldgen/copperOre") && Ic2Items.copperOre != null)
+        {
+            numVeins = 15 * baseScale / 64;
+            minY = 10;
+            maxY = 68; //Taken seaLevel is 64 see WorldProvider.getAverageGroundLevel() and WorldType.getMinimumSpawnHeight()
+            veinSize = 10;
+            float chance = (numVeins * veinSize) / ((maxY - minY + 1) * 256F);
+            registerOre(new RegisterOreMessage(Ic2Items.copperOre, new DistributionSquare(Math.max(0, minY - veinSize / 2), minY, maxY, Math.min(maxY + veinSize / 2, 255), chance)));
+        }
+
+        if(ConfigUtil.getBool(MainConfig.get(), "worldgen/tinOre") && Ic2Items.tinOre != null)
+        {
+            numVeins = 25 * baseScale / 64;
+            minY = 0;
+            maxY = 39;
+            veinSize = 6;
+            float chance = (numVeins * veinSize) / ((maxY - minY + 1) * 256F);
+            registerOre(new RegisterOreMessage(Ic2Items.tinOre, new DistributionSquare(Math.max(0, minY - veinSize / 2), minY, maxY, Math.min(maxY + veinSize / 2, 255), chance)));
+        }
+
+        if(ConfigUtil.getBool(MainConfig.get(), "worldgen/uraniumOre") && Ic2Items.uraniumOre != null)
+        {
+            numVeins = 20 * baseScale / 64;
+            minY = 0;
+            maxY = 63;
+            veinSize = 3;
+            float chance = (numVeins * veinSize) / ((maxY - minY + 1) * 256F);
+            registerOre(new RegisterOreMessage(Ic2Items.uraniumOre, new DistributionSquare(Math.max(0, minY - veinSize / 2), minY, maxY, Math.min(maxY + veinSize / 2, 255), chance)));
+        }
+
+        if(ConfigUtil.getBool(MainConfig.get(), "worldgen/leadOre") && Ic2Items.leadOre != null)
+        {
+            numVeins = 8 * baseScale / 64;
+            minY = 0;
+            maxY = 63;
+            veinSize = 4;
+            float chance = (numVeins * veinSize) / ((maxY - minY + 1) * 256F);
+            registerOre(new RegisterOreMessage(Ic2Items.leadOre, new DistributionSquare(Math.max(0, minY - veinSize / 2), minY, maxY, Math.min(maxY + veinSize / 2, 255), chance)));
+        }
+    }
+}
